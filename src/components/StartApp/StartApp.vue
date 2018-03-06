77d3dcf0ecf7e15690dcb27bccfbe69864da6d22
nishkaush <kaushish.nishant@hotmail.com>
2018-03-06 18:51:11 +1100

    setting up the space for tabbed content

--- src/components/StartApp/StartApp.vue
+++ src/components/StartApp/StartApp.vue
@@ -1,8 +1,10 @@
 <template>
 <v-container>
   <v-layout>
-    <v-flex>
+    <v-flex class="text-xs-center">
       <h1>Main App starts here, All About Patients</h1>
+      <v-btn>Search</v-btn>
+      <v-btn>Add New Patient</v-btn>
       <v-btn to="/">Go Back To Home</v-btn>
     </v-flex>
   </v-layout>
